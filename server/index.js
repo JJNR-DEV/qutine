e030921e7826ca548125dcb0f1c93776778ee596
Therese Lorraine <therese.abra@hotmail.com>
2020-11-21 18:51:47 +0100

    initial draft

--- server/index.js
+++ server/index.js
@@ -1,3 +1,21 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
+const port = 3000;
+const mongoose = require('mongoose');
+const authRoute = require('./routes/auth');
+
+mongoose.connect(process.env.DB_CONNECT, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => console.log('Connected to db!'))
+  .catch(err => console.log('Error occurred!')
+  );
+
+app.use(express.json());
+
+app.use('/api/user', authRoute);
+
+app.listen(port, () => console.log(`Server listening to port ${port}!`));
 
