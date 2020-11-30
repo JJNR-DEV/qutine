3cd4b0398340f9df2a5989a7e7b33d013d957435
Therese Abrahamsson <therese.abra@hotmail.com>
2020-11-30 21:54:09 +0100

    removed asterisk style

--- client/src/components/modal/Modal.jsx
+++ client/src/components/modal/Modal.jsx
@@ -8,7 +8,7 @@ const Modal = ({ open, toggle, children}) => {
   }
 
   return (
-    <div>
+    <div className="reset">
       <div className={open ? 'modal-overlay' : 'modal-overlay closed'} id="modal-overlay"/>
       <div className={open ? 'qutine-modal' : 'qutine-modal closed'} id="modal">
         <button className="modal-close" onClick={() => toggle(val => !val)}>Close</button>
