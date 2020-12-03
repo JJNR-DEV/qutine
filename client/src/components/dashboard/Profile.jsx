8582b06f25654c2d95a6dd249f7f3d95317e47fb
Therese Abrahamsson <therese.abra@hotmail.com>
2020-12-03 12:55:51 +0100

    clean up auth

--- client/src/components/dashboard/Profile.jsx
+++ client/src/components/dashboard/Profile.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import {useSelector} from "react-redux";
 // import {connect, useSelector} from 'react-redux';
 // const { user } = useSelector((state) => state.auth);
 
 const Profile = () => {
-  const { email } = JSON.parse(localStorage.getItem('user'));
+  const { user } = useSelector((state) => state.auth);
 
   return (
     <div className="profile">
-      <span>{email}</span>
+      <span>{user.email}</span>
     </div>
   );
 }
